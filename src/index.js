6d2eaa7f6bd53a8f1a41514954ea04f28cd2d5df
Roger Mayer <rmayer1984@gmail.com>
2020-03-23 21:49:25 -0500

    updated function with setState

--- src/index.js
+++ src/index.js
@@ -21,19 +21,24 @@ class App extends React.Component{
     // initialize state object
     constructor(props) {
         super(props);
+        //THIS IS THE ONLY TIME WE DO DIRECT ASSIGNMENT TO THIS.STATE
+        this.state = { lat: null };
+        window.navigator.geolocation.getCurrentPosition(
+            //function callback
+            (position) => {
+                //call setState to update function
+                this.setState({lat: position.coords.latitude});
+                console.log(position)
 
-        this.state = {lat: null};
-
+                //dont write this.state.lat = position.coords.latitude
+            },
+            (err) => console.log(err)
+        );
     }
 
     //must define render
     render(){
-        window.navigator.geolocation.getCurrentPosition(
-            //function callback
-            (position) => console.log(position),
-            (err) => console.log(err)
-        );
-        return <div>Latitude: </div>
+        return <div>Latitude: {this.state.lat} </div>
     }
 
 }
