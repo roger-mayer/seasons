5c940426dadfc8a35c5be1d917c51670118fbb48
Roger Mayer <rmayer1984@gmail.com>
2020-03-23 20:21:01 -0500

    starting state

--- src/index.js
+++ src/index.js
@@ -2,14 +2,31 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        //function callback
-        (position) => console.log(position),
-        (err) => console.log(err)
-    );
-    return <div>Latitude: </div>
-};
+
+//functional
+
+// const App = () => {
+//     window.navigator.geolocation.getCurrentPosition(
+//         //function callback
+//         (position) => console.log(position),
+//         (err) => console.log(err)
+//     );
+//     return <div>Latitude: </div>
+// };
+
+
+//component - Must be CLASS, extend React.Component, define 'render method that returns jsx
+class App extends React.Component{
+    render(){
+        window.navigator.geolocation.getCurrentPosition(
+            //function callback
+            (position) => console.log(position),
+            (err) => console.log(err)
+        );
+        return <div>Latitude: </div>
+    }
+
+}
 
 ReactDOM.render(
     <App />,
