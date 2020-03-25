3dd981c1dbbf63bc3b9d5457de96b8ad6812561a
Roger Mayer <rmayer1984@gmail.com>
2020-03-25 09:09:52 -0500

    added conditonal for error/loading

--- src/index.js
+++ src/index.js
@@ -40,13 +40,13 @@ class App extends React.Component{
 
     //must define render
     render(){
-        return (
-            <div>
-                Latitude: {this.state.lat}
-                <br />
-                Error: {this.state.errorMessage}
-            </div>
-        );
+            if (this.state.errorMessage && !this.state.lat){
+                return <div>Error: {this.state.errorMessage}</div>;
+            }
+        if (!this.state.errorMessage && this.state.lat){
+            return <div> Latitude: {this.state.lat}</div>;
+        }
+        return  <div>Loading....</div>;
     }
 
 }
