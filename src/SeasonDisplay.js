7f2e6d44721455b6c2282718e4e082166776a147
Roger Mayer <rmayer1984@gmail.com>
2020-03-25 20:54:44 -0500

    added ternary for season icon

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -12,11 +12,14 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = (props) =>{
     const season = getSeason(props.lat, new Date().getMonth());
     const text = season === 'winter' ? 'BURRRR!' : 'HOTTTT!';
+    const icon = season === 'winter' ? 'snowflake' : 'sun';
     console.log(season);
 
     return (
         <div>
-            {text}
+            <i className={`${icon} icon`} />
+            <h1>{text}</h1>
+            <i className={`${icon} icon`} />
         </div>
     );
 };
